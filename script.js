9c20b45d01c89560ec84249d4d93ddb1aae65a5f
shruti-mathews <shrutisusan@tamu.edu>
2024-06-03 23:38:13 +0400

    Fixing bugs

--- script.js
+++ script.js
@@ -3,6 +3,21 @@ const inputFields = document.querySelectorAll(".inp");
 const error = document.querySelector(".error");
 const progressValue = document.querySelector(".progress-value");
 const progressBar = document.querySelector(".progress-bar");
+const quote = document.querySelector(".motivation-quote");
+
+const allgoals = JSON.parse(localStorage.getItem('allGoals'))  || {};
+let completedGoals = Object.values(allgoals).filter((goal) => goal.completed).length;
+if(completedGoals === 1){
+    quote.innerText = "Well begun is half done!";
+}
+else if (completedGoals === 2){
+    quote.innerText = "Just a goal away, keep going! "
+}
+else if (completedGoals === 3){
+    quote.innerText = "Whoa! You just completed all the goals, time for chill :D !";
+}
+progressValue.style.width = `${completedGoals/ inputFields.length * 100}%`;
+progressValue.firstElementChild.innerText = `${completedGoals}/${inputFields.length} completed`;
 
 checkBoxes.forEach((checkBox) =>{
     let count = 0;
@@ -14,9 +29,22 @@ checkBoxes.forEach((checkBox) =>{
         if(allFieldsFilled){
             
             checkBox.parentElement.classList.toggle('completed');
+            const inputID = checkBox.nextElementSibling.id;
+            allgoals[inputID].completed = !allgoals[inputID].completed ;
+            localStorage.setItem('allGoals',JSON.stringify(allgoals))
+            completedGoals = Object.values(allgoals).filter((goal) => goal.completed).length;
+            if(completedGoals === 1){
+                quote.innerText = "Well begun is half done!";
+            }
+            else if (completedGoals === 2){
+                quote.innerText = "Just a goal away, keep going! "
+            }
+            else if (completedGoals === 3){
+                quote.innerText = "Whoa! You just completed all the goals, time for chill :D !";
+            }
             progressValue.style.display= 'flex';
-            progressValue.style.width= (count / 3) * 100;
-            console.log(progressValue.style.width)
+            progressValue.style.width= `${completedGoals/ 3 * 100}%`;
+            progressValue.firstElementChild.innerText = `${completedGoals}/${ inputFields.length} completed`;
         } 
         else{
             error.style.display='block';
@@ -25,7 +53,28 @@ checkBoxes.forEach((checkBox) =>{
 }) 
 
 inputFields.forEach((input) =>{
+    if(allgoals[input.id]){
+        input.value = allgoals[input.id].value;
+        if(allgoals[input.id].completed){
+            input.parentElement.classList.add('completed');
+        };
+
+    }
+    
     input.addEventListener('focus',(e)=>{
         error.style.display="none";
+    });
+
+    input.addEventListener('input', (e) =>{
+        if(allgoals[input.id] && allgoals[e.target.id].completed){
+            input.value = allgoals[e.target.id].value;
+            return;
+        }
+        localStorage.setItem('allGoals',JSON.stringify(allgoals))
+        allgoals[e.target.id] = {
+            value: e.target.value,
+            completed: false
+        };
+        
     });
 })
